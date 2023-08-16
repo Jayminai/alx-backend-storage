8d14c579177d8b50a95873d5d5517ab1464a39dd
Jayminai <georgegodstime62@gmail.com>
2023-08-16 23:04:03 +0100

    storage fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,21 +4,26 @@ import redis
 import requests
 r = redis.Redis()
 
-
 def get_page(url: str) -> str:
-    """uses the requests module to obtain the HTML content of a
-    particular URL and returns it"""
-    # r.set(f"cached:{url}", count)
-
+    """
+    Uses the requests module to obtain the HTML content of a particular URL and returns it.
+    """
+    # Increment and set expiration for URL access count
     if r.get(f"count:{url}"):
         r.incr(f"count:{url}")
         r.expire(f"count:{url}", 10)
     else:
         r.setex(f"count:{url}", 10, 1)
 
-    req = requests.get(url)
-    return req.text
+    # Check if HTML content is cached
+    cached_content = r.get(url)
+    if cached_content:
+        return cached_content.decode("utf-8")
 
+    # Fetch and cache HTML content
+    req = requests.get(url)
+    r.setex(url, 10, req.text)
+    return req.text
 
 if __name__ == "__main__":
     print(get_page('http://slowwly.robertomurray.co.uk'))
