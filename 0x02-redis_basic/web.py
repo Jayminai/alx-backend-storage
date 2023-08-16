a7eb6cd3f0d25c114ee3bf39a1130927ad7698e6
Jayminai <georgegodstime62@gmail.com>
2023-08-16 23:24:44 +0100

    storage fix-web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,73 +1,42 @@
 #!/usr/bin/env python3
-"""
-Web module
-"""
-
-import requests
+'''A module with tools for expiring request caching and tracking.
+'''
 import redis
+import requests
 from functools import wraps
+from typing import Callable
 
-class WebCache:
-    """
-    WebCache class for caching and tracking web pages
-    """
-    def __init__(self) -> None:
-        """
-        Initialize the WebCache instance and connect to Redis.
-        """
-        self._redis = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
-    def get_page(self, url: str) -> str:
-        """
-        Get the HTML content of a web page using the given URL.
-        Cache the result with an expiration time of 10 seconds.
-        Track the number of times the URL is accessed.
-
-        :param url: The URL of the web page.
-        :type url: str
-        :return: The HTML content of the web page.
-        :rtype: str
-        """
-        count_key = f'count:{url}'
-        page_content = self._redis.get(url)
-        
-        if page_content is None:
-            response = requests.get(url)
-            page_content = response.text
-            self._redis.setex(url, 10, page_content)
-        
-        self._redis.incr(count_key)
-        
-        return page_content
-
-def track_url_access(method):
-    """
-    A decorator to track the number of times a URL is accessed.
-    """
+def expiring_data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data with expiration.
+    '''
     @wraps(method)
-    def wrapper(self, url, *args, **kwargs):
-        count_key = f'count:{url}'
-        self._redis.incr(count_key)
-        return method(self, url, *args, **kwargs)
-    return wrapper
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output with expiration.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
+def get_page(url: str) -> str:
+    '''Returns the content of a URL after caching the request's response,
+    tracking the request, and using an expiration time.
+    '''
+    return requests.get(url).text
+
+# Decorate the get_page function with the expiring_data_cacher decorator
+get_page = expiring_data_cacher(get_page)
+
+# Test the decorated function
 if __name__ == "__main__":
-    web_cache = WebCache()
-
-    slow_url = "http://slowwly.robertomurray.co.uk/delay/5000/url/https://www.example.com"
-    page_content = web_cache.get_page(slow_url)
-    print(page_content)
-
-    page_content_cached = web_cache.get_page(slow_url)
-    print(page_content_cached)
-
-    fast_url = "https://www.example.com"
-    page_content_fast = web_cache.get_page(fast_url)
-    print(page_content_fast)
-
-    @track_url_access
-    def custom_get_page(self, url):
-        return self.get_page(url)
-
-    custom_page_content = custom_get_page(web_cache, slow_url)
-    print(custom_page_content)
+    slow_url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+    print(get_page(slow_url))
